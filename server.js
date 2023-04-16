38eb83ff2eaf3e84fba9298d4c4b41f68da321b4
Nephew20 <cofieldk20@gmail.com>
2023-04-16 18:31:31 -0400

    added route api notes

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
-// notes page
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//Notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  res.json(dbJSON)
 );
 
 // wildcard route that is used
@@ -20,7 +27,7 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
-
+// initialize the server
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
