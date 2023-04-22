a456df349cbf2b2a6f6a6f977e2a88fb377c43ce
Nephew20 <cofieldk20@gmail.com>
2023-04-22 11:03:48 -0400

    added POST route to add notes to the html

--- server.js
+++ server.js
@@ -18,9 +18,21 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//Route to generate the api notes from the db.json
 app.get('/api/notes', (req, res) =>
   res.json(dbJSON)
 );
+
+//Route to add a note to the express json 
+app.post('/api/notes', (req, res) => {
+  
+  console.info(`${req.method} method was received`)
+  
+ if (req.body && req.body.title) {
+  dbJSON.push(req.body)
+  res.json(`A new review has been added called ${req.body.title}`)
+ } 
+});
 
 // wildcard route that is used
 app.get('*', (req, res) =>
