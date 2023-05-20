582ebf3d99de06f0fa823a6f8efb98acd1a5f3bb
Nephew20 <cofieldk20@gmail.com>
2023-05-20 15:58:13 -0400

    able to write notes to the db.json file and created an id for the notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,20 +1,30 @@
 const notes = require('express').Router();
-const dbJSON = require('../Develop/db/db.json');
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
+const uuid = require('../helpers/uuid')
 
 //Route to generate the api notes from the db.json
-notes.get('/', (req, res) =>
-  res.json(dbJSON)
-);
+notes.get('/', (req, res) => {
+    console.info(`${req.method} method was received`)
+    readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 //Route to add a note to the express json 
 notes.post('/', (req, res) => {
   
   console.info(`${req.method} method was received`)
   
- if (req.body && req.body.title) {
-  dbJSON.push(req.body)
-  res.json(`A new review has been added called ${req.body.title}`)
- } 
+  const {title, text} = req.body
+
+  if (req.body) {
+    const newNote = {
+        title,
+        text,
+        note_id: uuid()
+    };
+
+    readAndAppend(newNote, './Develop/db/db.json');
+    res.json("A Note was added successfully")
+  }
 });
 
 module.exports = notes
