0162d52cce12389ee0b2e102311d50e52bf32d4e
Nephew20 <cofieldk20@gmail.com>
2024-11-16 19:13:39 -0500

    added comments

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,8 @@
 const notes = require('express').Router();
-const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
-const { json } = require('express');
 
+// Importing helper functions
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 //Route to get the notes from the db.json
 notes.get('/', (req, res) => {
@@ -11,16 +10,18 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
 
+//Route to select a specific note that is being looked for
 notes.get('/:id', (req, res) => {
     console.info(`${req.method} single ID method was received`);
 
     // id of the note in the database
     const noteId = req.params.id;
-    console.log('Note ID:', noteId);
 
-    // reads the database to find the note 
+    // helper function that reads the database then 
     readFromFile('./db/db.json')
         .then((data) => {
+
+            //Parse the data for mutability
             const jsonData = JSON.parse(data);
             console.log('JSON Data:', jsonData);
 
@@ -45,9 +46,10 @@ notes.post('/', (req, res) => {
 
     console.info(`${req.method} method was received`)
 
+    // Destructuring to target the title and text keys
     const { title, text } = req.body
 
-
+    // Check to see if the user put information in the text fields
     if (req.body) {
         const newNote = {
             title,
@@ -75,10 +77,9 @@ notes.delete('/:id', (req, res) => {
             .then((data) => {
                 const parsedlist = JSON.parse(data);
                 console.log('Parsed List:', parsedlist);
+    
+                const newList = parsedlist.filter((note) => note.id != noteId)
 
-                const newList = parsedlist.filter((note) => note.id !== noteId)
-
-                console.log("New List:", newList)
                 writeToFile('./db/db.json', newList)
                 
                 res.status(200).json()
